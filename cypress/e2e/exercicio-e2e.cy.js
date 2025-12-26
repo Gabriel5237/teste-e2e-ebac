7d15772b309fd8cab7bec08ad3980a10b9f24386
Gabriel Henrique <gabriel55237@gmail.com>
2025-12-26 07:43:12 -0300

    Exercicio e2e EBAC

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -1,4 +1,5 @@
 /// <reference types="cypress" />
+import produtosPage from "../support/page_objects/produtos.page";
 
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
   /*  Como cliente 
@@ -10,12 +11,25 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
       E validando minha compra ao final */
 
   beforeEach(() => {
-      cy.visit('/')
+    produtosPage.visitarUrl()
   });
 
   it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-      //TODO: Coloque todo o fluxo de teste aqui, considerando as boas práticas e otimizações
-      
+       let qtd = 4
+              produtosPage.buscarProduto('Autumn Pullie')
+              produtosPage.addProdutoCarrinho('M', 'Red', qtd)
+              cy.get('.woocommerce-message > .button').click()
+              cy.get('.checkout-button').click()
+              cy.get('#billing_first_name').type('Gabriel')
+              cy.get('#billing_last_name').type('Henrique')
+              cy.get('#billing_address_1').type('Rua teste, 1234')
+              cy.get('#billing_city').type('Lindoia')
+              cy.get('#billing_postcode').type('13950100')
+              cy.get('#billing_phone').type('40028922')
+              cy.get('#billing_email').type('gabrieltest@123.com')
+              cy.get('#terms').click()
+              cy.get('#place_order').click()
+             
   });
 
 
