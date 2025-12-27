39b72f07a9f2d5218fa1c143f29d5a3cdd3ad087
Gabriel Henrique <gabriel55237@gmail.com>
2025-12-27 06:02:42 -0300

    Exercicio e2e Ebac

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -29,7 +29,7 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
               cy.get('#billing_email').type('gabrieltest@123.com')
               cy.get('#terms').click()
               cy.get('#place_order').click()
-             
+             cy.get('.woocommerce-notice').should('contain' , 'Obrigado. Seu pedido foi recebido.')
   });
 
 
